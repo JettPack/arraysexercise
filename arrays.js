200444b9836fce9a78d707e185c1e321bf36a3ed
Jett M Palacpac <42420668+JettPack@users.noreply.github.com>
2018-12-17 22:50:28 -1000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,39 @@
+// 1 //
+let colours = [
+ "White",
+ "Red",
+ "Orange",
+ "Yellow",
+ "Green",
+]
+// 2 //
+console.log(colours[0])
+// 3 //
+console.log(colours[2])
+// 4 //
+colours[3] = "Ultraviolet"
+
+console.log(colours[3])
+// 5 //
+var fourthColour = colours[4]
+// 6 //
+colours[5] = "Blue"
+// 7 //
+colours.shift("Gray")
+// 8 //
+console.log(colours.length)
+// 9 //
+colours.pop("Blue")
+console.log(colours.pop())
+// 10 //
+for (i = 0; i < 5; i++) {
+	text += i + "<br>";}
+// 	11 //
+for (i = 0; i < 5; i++) {
+	text += i + ", " + colour "<br>";}
+	var colour = colours()
+// 12 //
+var lastColor = colours(colours.length())
+
+
+
